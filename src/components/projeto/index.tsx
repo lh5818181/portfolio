b87eaf1777bd3a267cffc14f936b879db1784a41
Luis Henrique <luish7572@gmail.com>
2025-01-29 09:03:16 -0300

    criando a seção do projetos e o componente do projeto

---
+++ src/components/projeto/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Titulo from "../titulo"
+import Paragrafo from "../Paragrafo"
+
+import { Card, LinkBotao } from "./styles"
+
+const Projeto = () => {
+  return (
+    <Card>
+      <Titulo fontSize={16}>Projeto - Lista de Tarefas</Titulo>
+      <Paragrafo tipo="secundario">
+        Lista de tarefas feita com o VueJs
+      </Paragrafo>
+      <LinkBotao>Visualizar</LinkBotao>
+    </Card>
+  )
+}
+
+export default Projeto
--- src/containers/projetos/index.tsx
+++ src/containers/projetos/index.tsx
@@ -1,9 +1,41 @@
 import React from "react"
 import Titulo from "../../components/titulo"
+import Projeto from "../../components/projeto"
+
+import { Lista } from "./styles"
 
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo> {/* Passando o valor de fontSize */}
+    <Lista>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+    </Lista>
   </section>
 )
 
